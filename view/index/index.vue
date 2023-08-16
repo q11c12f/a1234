a9e2e509a90d89aee57f802dad96230bf6175f1c
q11c12f <216102388@qq.com>
2023-08-16 15:08:36 +0800

    8.16下午提交

---
+++ view/index/index.vue
@@ -0,0 +1,15 @@
+<template>
+   <div class="topbox">
+      <h1>11223344</h1>
+   </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ view/login/login.vue
@@ -0,0 +1,16 @@
+<template>
+   <div class="box">
+      <img src="" alt="">
+      <h1>{{msg}}</h1>
+   </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
